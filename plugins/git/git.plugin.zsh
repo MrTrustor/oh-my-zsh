0d1305da19c0d229b2840033ae3d0445936df58d
Théo Chamley <theo.chamley@gmail.com>
2012-12-06 21:21:39 +0100

    current branch returns nothing if in detached head mode. Dealt with in mrtrustor theme

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -73,8 +73,7 @@ alias gsd='git svn dcommit'
 # Usage example: git pull origin $(current_branch)
 #
 function current_branch() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
-  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  ref=$(git symbolic-ref HEAD 2> /dev/null) ||  return
   echo ${ref#refs/heads/}
 }
 
